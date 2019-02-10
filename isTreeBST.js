8094e1416f6c41ea9cf84f4d36168ff22eaeb016
Sarah Kaplan <skaplan520@gmail.com>
2019-02-10 10:31:34 -0800

    added solution for CCI 4.5 is binary tree a BST?

---
+++ isTreeBST.js
@@ -0,0 +1,36 @@
+// CCI 4.5 Is a binary tree a Binary Search Tree?
+
+const { NodeBinaryTree } = require('./treesWarmups');
+
+//example tree
+let root = new NodeBinaryTree(7);
+root.appendLeft(5);
+root.appendRight(8);
+root.left.appendLeft(3);
+root.left.appendRight(9);
+root.right.appendRight(10);
+// console.log(root);
+
+function validateBST(root, min = -Infinity, max = Infinity) {
+  if (root.val < min || root.val > max) {
+    return false;
+  }
+
+  if (root.left) {
+    let result = validateBST(root.left, min, root.val);
+    if (!result) {
+      return false;
+    }
+  }
+
+  if (root.right) {
+    let result = validateBST(root.right, root.val, max);
+    if (!result) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(validateBST(root)); //false
